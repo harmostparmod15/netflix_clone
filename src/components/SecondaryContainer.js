57c0e20a86cb5a67ed11b8ecf93dbe84260f04ab
= <=>
2024-02-05 13:12:31 +0530

    secondary container with movie list && usePopularMovies && useTopRatedMovies

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,33 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store?.movies);
+
+  return (
+    movies && (
+      <div className=" bg-black ">
+        <div className="-mt-52 pl-12 relative z-20 ">
+          <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
+          <MovieList title={"Popular"} movies={movies?.popularMovies} />
+        </div>
+
+        {/* 
+ movielList - popular
+   movieCards * n
+ movieList - now playing
+    movieCards * n
+ movieList - trending
+    movieCards * n
+ movieList - horror
+    movieCards * n
+
+*/}
+      </div>
+    )
+  );
 };
 
 export default SecondaryContainer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZWU4MzFkNmNhOWRlZGIyMDZmYjJiMTAxMmIyMGE0MCIsInN1YiI6IjY1YzA1NWYxNDM5OTliMDE4NGM5M2Y1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LpGYydqy-dBfuLs5AsqAf02HhEtDMRUtbMJuP4x15QU",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/original/";
